d020c64f5390caee8ef7778fb68d9611ddb606ab
Luiz <luiz.cl@live.com>
2020-10-02 15:31:23 -0300

    Find category by id, exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResources.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,13 @@ public class CategoryResources {
 		
 		return ResponseEntity.ok(list);
 		//return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO>findById(@PathVariable long id){
+		
+		return ResponseEntity.ok().body(service.findById(id));
+				
 	}
 	
 }
