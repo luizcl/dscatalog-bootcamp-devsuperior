d282aa50184b2acf9ed930a31dacfed81b9a3ec3
Luiz Moraes <luiz.cl@live.com>
2020-10-09 12:09:20 -0300

    State component pt 1

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import Alert from './Alert'
+import React, {useState} from 'react';
 
 const App = () => { 
+    const [counter, setCounter] = useState(0);
+
     return (
         <div className="container mt-5">
-            <Alert text="Luiz"/>   
-            <Alert/>   
-            <Alert text="Luiz"/>   
-            <Alert text="Luiz"/>   
-            <Alert text="Luiz"/>   
+
+            <button 
+                className="btn btn-primary mr-5"
+                onClick={() => setCounter(counter + 1)}
+            >
+                +
+            </button>
+            <span>
+               {counter}
+            </span>
+            <button 
+                className="btn btn-primary ml-5"
+                onClick={() => setCounter(counter - 1)}
+            >
+                -
+            </button>
+            {counter > 5 && <h1>O valor é maior que 5</h1>}
+            {counter < 0 && setCounter(0)}
         </div>
     )
 }
