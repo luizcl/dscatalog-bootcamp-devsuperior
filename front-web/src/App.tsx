99fe85c6cf20989f438d5a5507758a271352f8d0
Luiz <luiz.cl@live.com>
2020-10-08 22:45:48 -0300

    Part 2, continuing last class

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Alert from './Alert'
 
 const App = () => { 
-    return <h1>Hello App</h1>
+    return (
+        <div className="container mt-5">
+            <Alert text="Luiz"/>   
+            <Alert/>   
+            <Alert text="Luiz"/>   
+            <Alert text="Luiz"/>   
+            <Alert text="Luiz"/>   
+        </div>
+    )
 }
 
 export default App;
-
