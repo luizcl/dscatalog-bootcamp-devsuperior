212ebf87aad3904f18b1c877e4589c9551657585
Luiz Claudio Moura Moraes <luizmoraes@demprel.local>
2020-10-20 07:42:48 -0300

    Pagination complete

--- front-web/src/core/components/Pagination/index.tsx
+++ front-web/src/core/components/Pagination/index.tsx
@@ -13,18 +13,28 @@ type Props = {
 const Pagination = ({totalPages, activePage, onChange}:Props) => {
     const items = generateList(totalPages);
 
+    const firstPage = (activePage === 0);
+    const lastPage = (activePage === (totalPages - 1));
+
+    const previousClass = firstPage ? 'page-inactive' : 'page-active';
+    const nextClass = lastPage ? 'page-inactive' : 'page-active';
+
     return (
         <div className="pagination-container">
-            <ArrowIcon className="pagination-previous" />
+            <ArrowIcon 
+                className={`pagination-previous ${previousClass}`}
+                onClick={() => onChange(activePage - 1)}/>
             {items.map(item => (
                 <div 
                     className={`pagination-item ${item === activePage ? 'active' : ''}`}
                     key={item}
                     onClick={() => onChange(item)}>
-                        {item + 1} 
+                        {item + 1}
                 </div>
             ))}
-            <ArrowIcon className="pagination-next" />
+            <ArrowIcon 
+                className={`pagination-next ${nextClass}`}
+                onClick={() => onChange(activePage + 1)}/>
         </div>
     );
 }
